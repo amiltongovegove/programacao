6e49fd6fc8703d8e6594b4e638cac18ac12a73b5
Amilton Gove <155755000+amiltongovegove@users.noreply.github.com>
2024-03-17 00:16:12 +0200

    Create ambiente.js

---
+++ exercicios/cursojs/aula14/ambiente.js
@@ -0,0 +1,16 @@
+/*   
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c= c + 1
+}
+*/
+//------------------------------------
+
+console.log('Vai começar...')
+for(var c =1; c <= 5; c++) {
+    console.log(c) 
+}
+console.log('FIM!')
+    
+
