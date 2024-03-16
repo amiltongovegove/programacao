0ba8d6dc0086ea04ec9c2f9a29c715c35644eee5
Amilton Gove <155755000+amiltongovegove@users.noreply.github.com>
2024-03-16 16:19:26 +0200

    Create ex008.js

---
+++ exercicios/cursojs/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // Condição simeples
+    console.log('Você ultrassou a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre o cinto de segurança!')
