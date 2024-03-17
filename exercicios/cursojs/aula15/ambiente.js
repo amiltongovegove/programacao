a4980905940592471148f7ee5e0c7b201576d305
Amilton Gove <155755000+amiltongovegove@users.noreply.github.com>
2024-03-17 10:47:55 +0200

    Update ambiente.js

--- exercicios/cursojs/aula15/ambiente.js
+++ exercicios/cursojs/aula15/ambiente.js
@@ -5,3 +5,9 @@ num.sort()
 console.log(num)
 console.log(`O valor tem ${num.length} posições`)
 console.log(`O primeiro valor do vector é ${num[0]}`)
+let pos = num.indexOf(8)
+if(pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
