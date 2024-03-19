df6602eceac0dda0db472ee7229e3e8b52d95f97
Amilton Gove <155755000+amiltongovegove@users.noreply.github.com>
2024-03-19 23:04:07 +0200

    Update script.js

--- projectos/exjs02/script.js
+++ projectos/exjs02/script.js
@@ -1,20 +1,9 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
-    if (hora >= 0 && hora < 12) {
-        //BOM DIA!
-        img.src = 'manha.png'
-        document.body.style.background = 'rgb(52, 179, 115)'
-    } else if (hora >= 12 && hora < 18) {
-        //BOA TARDE!
-        img.src = 'tarde.png'
-        document.body.style.background = 'rgb(218, 115, 107)'
-    } else {
-        //BOA NOITE!
-        img.src = 'noite.png'
-        document.body.style.background = 'rgba(131, 139, 158, 0.644)'
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value > ano)) {
+        window.alert('[Erro] verifique os dados e tente novamente!')
     }
 }
