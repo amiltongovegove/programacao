623ac5b620250f6846dab61c729901c44ac567a9
Amilton Gove <155755000+amiltongovegove@users.noreply.github.com>
2024-03-20 17:54:18 +0200

    Update script.js

--- projectos/exjs02/script.js
+++ projectos/exjs02/script.js
@@ -3,7 +3,19 @@ function verificar() {
     var ano = data.getFullYear
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value > ano)) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[Erro] verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        if (fsex[0].chacked) {
+            genero = 'Homem'
+        } else if (fsex[1].chacked) {
+            genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
     }
 }
